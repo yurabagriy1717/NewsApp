fb90ba4c53dc2195bd7677462c53ca87b0c347b2
Yura <yurabahrii17@gmail.com>
2025-11-02 14:40:40 +0200

    Initial commit for NewsApp

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Юрій Багрій on 02.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
